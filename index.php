22bb25c70cf565fb57afcb9184eae9a7735fe9b0
osk0007 <jirioskera33@gmail.com>
2016-10-05 10:21:17 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="">
+<head>
+     
+    
+    <title>PAW</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="robots" content="index, follow">
+	<meta name="revisit-after" content="7 days">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	
+</head>
+<body>
+<?php
+	print "</p><b2><left>žůžo</left></b2></p>";
+	print "<ul>";
+	for ($i = 0; $i<10; $i++)  { 
+		print  "<li><b>hhhh".$i."</b></li>" ;
+	}
+	
+	print "<b1><i>mám hlad</i>  - Jirka</b1>";
+	?>
+
+</body>
+</html>
